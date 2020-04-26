33a68e333b425dbc1d0fd65fe1f64c4a2be99d3c
KrisztaAnna <krisztina.baranyai@gmail.com>
2020-04-26 16:04:55 +0100

    add finalise_line and add_lines

--- main.py
+++ main.py
@@ -3,20 +3,35 @@ from sys import argv
 
 
 def get_files(directory):
+    files = []
     for entry in os.scandir(directory):
         if entry.path.endswith(".csv") and entry.is_file():
-            print(entry.path)
+            files.append(entry.path)
+    return files
 
 
-def get_file_content(filename):
-    with open(filename) as file:
+# This adds the location and name of the original file a the end of each line
+def finalise_line(line, filename):
+    return line.strip("\n") + "," + filename + "\n"
+
+
+def get_file_content(input_file):
+    with open(input_file) as file:
         data = file.readlines()
         del data[0]
         return data
 
 
+def add_lines(directory, output_file):
+    files = get_files(directory)
+    with open(output_file, "a") as file:
+        for filename in files:
+            for line in get_file_content(filename):
+                file.write(finalise_line(line, filename))
+
+
 def main():
-    get_files(argv[1])
+    add_lines(argv[1], argv[2])
 
 
 if __name__ == "__main__":
